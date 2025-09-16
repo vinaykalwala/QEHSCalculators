932b48049afc07223d355451113b2583d644ae9a
vishnu7799 <vishnugorrela2003@gmail.com>
2025-09-16 10:36:45 +0530

    viewsupdate16-09-2025

--- QehsCalculators/views.py
+++ QehsCalculators/views.py
@@ -2387,7 +2387,7 @@ def environment_retention_time_in_settling_zone_t_calculator(request):
 @login_required
 @subscription_required(plan_type="corporate")
 def environment_rankins_equation_filter_efficiency_calculator(request):
-    return render(request, 'qehsfcalculators/environment/rankins_equation_filter_efficiency.html')
+    return render(request, 'qehsfcalculators/environment/rankins_equation_filter_efficiency.html', {'title': 'Rankins Equation - Filter Efficiency Calculator'})
 
 @login_required
 @subscription_required(plan_type="corporate")
